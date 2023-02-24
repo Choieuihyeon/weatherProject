1a4a5eeacc231d45b5ad48184cbb53d804949c3f
CHOI <ceh201312@naver.com>
2023-02-24 22:03:26 +0900

    final commit

---
+++ src/main/java/zerobase/weather/repository/JdbcMemoRepository.java
@@ -0,0 +1,46 @@
+package zerobase.weather.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import zerobase.weather.domain.Memo;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcMemoRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcMemoRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Memo save(Memo memo) {
+        String sql = "insert into memo values (?,?)";
+        jdbcTemplate.update(sql, memo.getId(), memo.getText());
+        return memo;
+    }
+
+    public List<Memo> findAll() {
+        String sql = "select * from memo";
+        return jdbcTemplate.query(sql, memoRowMapper());
+    }
+
+    public Optional<Memo> findById(int id) {
+        String sql = "select * from memo where id = ?";
+        return jdbcTemplate.query(sql, memoRowMapper(), id).stream().findFirst();
+    }
+
+    private RowMapper<Memo> memoRowMapper() {
+        // ResultSet (database에서 가져오면 이형태)
+        // {id = 1, text = 'this is memo~'} 이런 식으로 가져오기 때문에 RowMapper로 바꿔줘야함
+        return (rs, rowNum) -> new Memo(
+                rs.getInt("id"),
+                rs.getString("text")
+        );
+    }
+}
